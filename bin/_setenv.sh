6ea6da9305a398f19e5410fa5c1a6cab7868fcbb
Xavier Yvez <xavieryvez@gmail.com>
2016-02-14 09:53:31 +0200

    real live contract addr, placeholder live coinbase

--- bin/_setenv.sh
+++ bin/_setenv.sh
@@ -11,10 +11,10 @@ source .setenv.sh
 
 export ADDR_COINBASE_DEV=0x63cf90d3f0410092fc0fca41846f596223979195
 export ADDR_COINBASE_TST=$ADDR_COINBASE_DEV
-export ADDR_COINBASE_LVE=0xbf885e2b55c6bcc84556a3c5f07d3040833c8d00
+export ADDR_COINBASE_LVE=$ADDR_COINBASE_TST
 
 export ADDR_LOTTERY_DEV=0x905b2ea4789b38742cc9f86eb54417130f8f7eee
-export ADDR_LOTTERY_TST=0x3a07c9bb35879c0d949fff5d123e66f23d049961
-export ADDR_LOTTERY_LVE=$ADDR_LOTTERY_TST
+export ADDR_LOTTERY_TST=0xa6f0e7bd469c00776cd783650851fb43b8763716
+export ADDR_LOTTERY_LVE=0xfe147b94a4ee2285db73d7897438a94a97be64c2
 
 export SERVICE_TMPL="[Unit]\nDescription=www service\n\n[Install]\nWantedBy=multi-user.target\n\n[Service]\nKillMode=process\nRestart=on-failure\nRestartSec=15s\nExecReload=/bin/kill -HUP \\\$MAINPID\n"
