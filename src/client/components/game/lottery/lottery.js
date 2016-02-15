ca84ee77da93d3b2c8f47fcd58afa3ad5ec8e865
Xavier Yvez <xavieryvez@gmail.com>
2016-02-15 10:44:28 +0200

    don't add duplicated entries

--- src/client/components/game/lottery/lottery.js
+++ src/client/components/game/lottery/lottery.js
@@ -33,6 +33,11 @@
 
         for (let idx = 0; idx < this.entries.length; idx++) {
           const _entry = this.entries[idx];
+
+          if (entry.addr === _entry.addr && entry.txhash === _entry.txhash) {
+            return;
+          }
+
           const newround = entry.round > _entry.round;
           const newentry = (entry.round === _entry.round) && (entry.total > _entry.total);
 
