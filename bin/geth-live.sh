7e27a575207255d6f7b973260a7856207999c63b
Xavier Yvez <xavieryvez@gmail.com>
2016-02-16 10:25:43 +0200

    set CORS domain

--- bin/geth-live.sh
+++ bin/geth-live.sh
@@ -4,5 +4,6 @@ source bin/_setenv.sh
 
 geth \
   --rpc \
+  --rpccorsdomain "http://localhost:8887" \
   --unlock $ADDR_COINBASE_LVE \
   --password .password.$ADDR_COINBASE_LVE
--- bin/geth-test.sh
+++ bin/geth-test.sh
@@ -4,6 +4,7 @@ source bin/_setenv.sh
 
 geth \
   --rpc \
+  --rpccorsdomain "http://localhost:8887" \
   --unlock $ADDR_COINBASE_TST \
   --password .password.$ADDR_COINBASE_TST \
   --testnet \
