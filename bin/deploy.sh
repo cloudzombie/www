148e16ee7053499b7c94938b42fa7d2a1959048a
Xavier Yvez <xavieryvez@gmail.com>
2016-02-17 08:26:05 +0200

    cleanup comment (left-over)

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -21,7 +21,6 @@ fi
 
 GETH_PASS_FILE=".password.$ADDR_COINBASE"
 
-# INSTANCE_NAME="" WS_SECRET="" WS_SERVER="" node app.js
 SERVICE_EXEC_GETH="ExecStart=/usr/bin/geth --rpc --datadir /root/.ethereum --unlock $ADDR_COINBASE --password /root/$GETH_PASS_FILE $GETH_NET"
 SERVICE_EXEC_NODE="WorkingDirectory=/www/dist\nExecStart=/usr/bin/nodejs server.js --port 80 --contract-fifty $ADDR_FIFTY --contract-lottery $ADDR_LOTTERY"
 SERVICE_EXEC_MONITOR="Environment=INSTANCE_NAME=$MONITOR_INSTANCE\nEnvironment=WS_SECRET=$MONITOR_KEY\nEnvironment=WS_SERVER=$MONITOR_SERVER\nEnvironment=CONTACT_DETAILS=$MONITOR_CONTACT\nWorkingDirectory=/www/gethmonitor\nExecStart=/usr/bin/nodejs app.js"
