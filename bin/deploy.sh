9721cc2b1700494c98a1b9027c3e05db0a0d1b4f
Xavier Yvez <xavieryvez@gmail.com>
2016-02-26 07:44:13 +0200

    HACK: restart get on interval (close to tears)

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -27,6 +27,9 @@ SERVICE_EXEC_GETH="ExecStart=/usr/bin/geth --rpc --datadir /root/.ethereum --unl
 SERVICE_EXEC_NODE="WorkingDirectory=/www/dist\nExecStart=/usr/bin/nodejs server.js --port 80 --contract-dice $ADDR_DICE --contract-fifty $ADDR_FIFTY --contract-lottery $ADDR_LOTTERY"
 SERVICE_EXEC_MONITOR="Environment=INSTANCE_NAME=$MONITOR_INSTANCE\nEnvironment=WS_SECRET=$MONITOR_KEY\nEnvironment=WS_SERVER=$MONITOR_SERVER\nEnvironment=CONTACT_DETAILS=$MONITOR_CONTACT\nWorkingDirectory=/www/gethmonitor\nExecStart=/usr/bin/nodejs app.js"
 
+SERVICE_TIMER_RESTART="[Unit]\nDescription=Geth restart\n[Timer]\nOnBootSec=20min\nOnUnitActiveSec=20min\nUnit=wwwgethrestart.service\n[Install]\nWantedBy=multi-user.target\n"
+SERVICE_EXEC_RESTART="[Unit]\nDescription=Geth restart\n\n[Service]\nType=simple\nExecStart=/bin/systemctl restart wwwgeth\n"
+
 echo  "Creating dist/"
 gulp clean
 gulp --minimize
@@ -52,6 +55,7 @@ ssh -i $TLF_KEY root@$TLF_HOST << DEPLOYEND
   echo
 
   echo "Stopping systemd www*"
+  systemctl stop wwwgethtimer
   systemctl stop wwwnode
   systemctl stop wwwgethmon
   systemctl stop wwwgeth
@@ -59,6 +63,14 @@ ssh -i $TLF_KEY root@$TLF_HOST << DEPLOYEND
 
   echo "Creating Geth password file"
   printf "$GETH_PASS\n" > /root/$GETH_PASS_FILE
+  echo
+
+  echo "Creating wwwgethtimer.timer"
+  printf "$SERVICE_TIMER_RESTART" > /lib/systemd/system/wwwgethtimer.timer
+  echo
+
+  echo "Creating wwwgethrestart.service"
+  printf "$SERVICE_EXEC_RESTART" > /lib/systemd/system/wwwgethrestart.service
   echo
 
   echo "Creating wwwgeth.service"
@@ -88,6 +100,7 @@ ssh -i $TLF_KEY root@$TLF_HOST << DEPLOYEND
   systemctl start wwwgeth
   systemctl start wwwgethmon
   systemctl start wwwnode
+  systemctl start wwwgethtimer.timer
   echo
 DEPLOYEND
 
