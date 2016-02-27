c602354d144d358c404dec138f4cc7208677dcc4
Xavier Yvez <xavieryvez@gmail.com>
2016-02-27 13:56:41 +0200

    expose api

--- src/server/route/api/game/index.js
+++ src/server/route/api/game/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router(); // eslint-disable-line new-cap
 
+router.use('/box', require('./box'));
 router.use('/dice', require('./dice'));
 router.use('/fifty', require('./fifty'));
 router.use('/lottery', require('./lottery'));
