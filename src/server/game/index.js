4415bf418e7aafb3bf93e118100ad3e2ff9f8e81
Xavier Yvez <xavieryvez@gmail.com>
2016-02-27 15:32:19 +0200

    use glob to auto-init all games

--- src/server/game/index.js
+++ src/server/game/index.js
@@ -1,16 +1,23 @@
-const logger = require('../lib/logger');
+const _ = require('lodash');
+const glob = require('glob');
 
-const dice = require('./dice');
-const fifty = require('./fifty');
-const lottery = require('./lottery');
+const logger = require('../lib/logger');
 
 const init = function() {
   logger.log('Game', 'init', 'initializing games');
 
-  box.init();
-  dice.init();
-  fifty.init();
-  lottery.init();
+  glob('*.js', { cwd: __dirname }, (error, files) => {
+    if (error) {
+      logger.error('Game', 'init', error);
+      return;
+    }
+
+    _.each(files, (file) => {
+      if (file.indexOf('index.js') === -1) {
+        require(`./${file}`).init();
+      }
+    });
+  });
 
   logger.log('Game', 'init', 'initialized all games');
 };
